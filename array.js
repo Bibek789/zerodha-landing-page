fcfab79b79aa5ff1997f0f0e779065fe293cbeb8
Bibek789 <110345163+Bibek789@users.noreply.github.com>
2024-02-08 20:40:41 +0530

    conversion of datatypes

---
+++ array.js
@@ -0,0 +1,8 @@
+function Sum(a, b){
+    const sumValue = a + b;
+    return sumValue;
+}
+const value = Sum(1, 2)
+const value2 = Sum(1, 10)
+console.log(value);
+
---
+++ firstjs.js
@@ -0,0 +1,7 @@
+//1. write the program to greet a person given their first  and last name
+let answer = 0;
+
+for (let i = 0;i<=1000; i++){
+    answer  = answer + i;
+}
+console.log(answer);
